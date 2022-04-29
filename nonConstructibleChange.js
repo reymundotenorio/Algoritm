314fc78267753d4a0be37c4606040e526cb3980a
Reymundo Tenorio <reymundo.tenorio@jolt.com>
2022-04-29 14:41:56 -0600

    Updating Method

--- nonConstructibleChange.js
+++ nonConstructibleChange.js
@@ -1,28 +1,26 @@
-// const nonConstructibleChange = () => {
-//   let arr = [5, 7, 1, 1, 2, 3, 22];
+const nonConstructibleChange = () => {
+  let arr = [5, 7, 1, 1, 2, 3, 22];
 
-//   const arrAsc = arr.sort((a, b) => b - a);
+  const arrAsc = arr.sort((a, b) => b - a);
 
-//   console.log(arr);
+  console.log(arr);
 
-//   let iterator = 1;
-//   let flag = true;
+  let iterator = 1;
+  let flag = true;
 
-//   while (flag) {
-//     if (arr.includes(iterator)) {
-//       iterator += 1;
-//     } else {
-//       const firstIndex = arrAsc.findIndex((num) => num < iterator);
-//       const nearNumber = arrAsc[firstIndex];
+  while (flag) {
+    if (arr.includes(iterator)) {
+      iterator += 1;
+    } else {
+      const firstIndex = arrAsc.findIndex((num) => num < iterator);
+      const nearNumber = arrAsc[firstIndex];
 
-//       const newArr = arrAsc.filter((item, index) => index !== firstIndex);
+      const newArr = arrAsc.filter((item, index) => index !== firstIndex);
 
-//       if (newArr.includes(iterator - nearNumber)) {
-//         iterator += 1;
-//       }
-//       else{
-
-//       }
-//     }
-//   }
-// };
+      if (newArr.includes(iterator - nearNumber)) {
+        iterator += 1;
+      } else {
+      }
+    }
+  }
+};
